f3894e39b4385a82b83e6992800861a2db68ccb7
Jakub Kapusta <jakubkapusta39@gmail.com>
2019-12-21 19:37:33 +0100

    Added ResponseEntity in SeatPriceController

--- src/main/java/pl/edu/wat/airline/controllers/SeatPriceController.java
+++ src/main/java/pl/edu/wat/airline/controllers/SeatPriceController.java
@@ -1,6 +1,8 @@
 package pl.edu.wat.airline.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.wat.airline.dtos.SeatPriceDto;
 import pl.edu.wat.airline.services.SeatPricesServiceImpl;
@@ -17,12 +19,12 @@ public class SeatPriceController {
     }
 
     @GetMapping("all_seat_price")
-    public Iterable<SeatPriceDto> getAllSeats(){
-        return seatPricesServiceImpl.findAll();
+    public ResponseEntity<Iterable<SeatPriceDto>> getAllSeats(){
+        return new ResponseEntity<>(seatPricesServiceImpl.findAll(), HttpStatus.OK);
     }
 
     @PostMapping
-    public SeatPriceDto addSeatPrice(@RequestBody SeatPriceDto seatPriceDto) {
-        return seatPricesServiceImpl.addSeatPrice(seatPriceDto);
+    public ResponseEntity<SeatPriceDto> addSeatPrice(@RequestBody SeatPriceDto seatPriceDto) {
+        return new ResponseEntity<>(seatPricesServiceImpl.addSeatPrice(seatPriceDto), HttpStatus.OK);
     }
 }
